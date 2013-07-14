7db77b834368d905b07de3debaa33c75d6904d33
Sloy <rafa.vazsan@gmail.com>
2013-07-14 14:43:45 +0200

    Cambiada estructura del layout "texto + lista" en la pantalla de Controles.
    Esto supone:
    - Simplifica la jerarquía de vistas, haciéndola menos pesada.
    - Al cambiar los márgenes y paddings, la lista se ajusta a los bordes de la pantalla, para que el "sangrado" y la barra de scroll aparezcan en los bordes, y no separados de estos. Es un detalle tonto pero suelo fijarme mucho en esas cosas :P
    - Evita el uso del método estático setListViewHeightBasedOnChildren(), que al forzar la creación y medición de TODAS las vistas de la lista es tremendamente costoso. En listas simples no se nota tanto, pero en listas con elementos más complejos o más largas es apreciable (ej: la lista de controles, al seleccionar Controles en el menú se nota como éste "salta" unos milisegundos).

--- src/com/hitglynorthz/cubeworldbasics/ControlsFragment.java
+++ src/com/hitglynorthz/cubeworldbasics/ControlsFragment.java
@@ -10,34 +10,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+import android.widget.*;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class ControlsFragment extends Fragment implements OnItemClickListener {
 	String[] controls_items;
-	
+
 	@Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState)
-    {       
+    {
         View view = inflater.inflate(R.layout.controls_layout, container,false);
-        
+
         //List Equipment
         final ListView listControls = (ListView) view.findViewById(R.id.listControls);
+
+        // Coloca el texto como Header de la lista, para que haga Scroll junto con ella.
+        // Evita los problemas de meter un ListView en un ScrollView, y ahorra medir los hijos para decidir la altura.
+        listControls.setHeaderDividersEnabled(true);
+        TextView header = (TextView) LayoutInflater.from(getActivity()).inflate(R.layout.item_list_about, listControls, false);
+        header.setText(R.string.controls_about);
+        header.setLayoutParams(new ListView.LayoutParams(ListView.LayoutParams.MATCH_PARENT, ListView.LayoutParams.WRAP_CONTENT));
+        listControls.addHeaderView(header, null, false);
+
         controls_items = getResources().getStringArray(R.array.controls_items);
-        ArrayAdapter<String> adapterEquipment = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, controls_items);
+        ArrayAdapter<String> adapterEquipment = new ArrayAdapter<String>(getActivity(), R.layout.item_list_control, controls_items);
         listControls.setAdapter(adapterEquipment);
-        listControls.setOnItemClickListener(new OnItemClickListener(){
-            public void onItemClick(AdapterView<?> parent, View view, int position,long id){
+        listControls.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             	/*Intent it = new Intent(getActivity(), SingleListItem.class);
                 it.putExtra("title", controls_items[position]);
                 startActivity(it);*/
             }
         });
-        ListSizeHelper.setListViewHeightBasedOnChildren(listControls);
         
         return view;
     }
